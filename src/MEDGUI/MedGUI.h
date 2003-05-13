a6ac62a3b7e07218a543797c8c8e48778e513a8c
jroy <jroy>
2003-05-13 10:43:54 +0000

    Version ok de MED, avec gestion des idl (copie depuis $KERNEL_ROOT_DIR/idl/salome pour les idl communs avec KERNEL).

--- src/MEDGUI/MedGUI.h
+++ src/MEDGUI/MedGUI.h
@@ -12,7 +12,7 @@
 
 #include <SALOMEconfig.h>
 #include CORBA_CLIENT_HEADER(Med_Gen)
-#include CORBA_CLIENT_HEADER(SMESH_Mesh)
+//#include CORBA_CLIENT_HEADER(SMESH_Mesh)
 #include CORBA_SERVER_HEADER(SALOMEDS_Attributes)
 
 class MedGUI: public QObject
@@ -50,7 +50,7 @@ public:
   static Engines::Med_Gen_ptr InitMedGen(QAD_Desktop* parent);
 
   static bool DumpMesh( SALOME_MED::MESH_var aMesh );
-  static bool DumpSubMesh( SMESH::SMESH_subMesh_ptr aSubMesh ) ;
+  //  static bool DumpSubMesh( SMESH::SMESH_subMesh_ptr aSubMesh ) ;
   static bool DumpSubMesh( SALOME_MED::FAMILY_var Fam ) ;
 
   static void setOrb();
