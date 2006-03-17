3329e61180a5bb844e7a7621536fa5940a551a34
eap <eap@opencascade.com>
2006-03-17 10:09:22 +0000

    PAL10556. convert groups and attributes in blankCopy()

--- src/MedClient/src/FAMILYClient.cxx
+++ src/MedClient/src/FAMILYClient.cxx
@@ -34,33 +34,33 @@ void FAMILYClient::blankCopy(bool blankSupport)
   if (blankSupport)
     SUPPORTClient::blankCopy();
 
-//   setIdentifier(IOR_Family->getIdentifier());
+  setIdentifier(IOR_Family->getIdentifier());
 
-//   int nAttr, nGr, n;
-//   int *i;
-//   std::string *s;
+  int nAttr, nGr, n;
+  int *i;
+  std::string *s;
 
-//   nAttr = IOR_Family->getNumberOfAttributes();
-//   setNumberOfAttributes(nAttr);
+  nAttr = IOR_Family->getNumberOfAttributes();
+  setNumberOfAttributes(nAttr);
 
-//   convertCorbaArray(i, n, IOR_Family->getAttributesIdentifiers());
-//   ASSERT(n == nAttr);
-//   setAttributesIdentifiers(i);
+  convertCorbaArray(i, n, IOR_Family->getAttributesIdentifiers());
+  ASSERT(n == nAttr);
+  setAttributesIdentifiers(i);
 
-//   convertCorbaArray(i, n, IOR_Family->getAttributesValues());
-//   ASSERT(n == _numberOfAttribute);
-//   setAttributesValues(i);
+  convertCorbaArray(i, n, IOR_Family->getAttributesValues());
+  ASSERT(n == _numberOfAttribute);
+  setAttributesValues(i);
 
-//   convertCorbaArray(s, n, IOR_Family->getAttributesDescriptions());
-//   ASSERT(n == _numberOfAttribute);
-//   setAttributesDescriptions(s);
+  convertCorbaArray(s, n, IOR_Family->getAttributesDescriptions());
+  ASSERT(n == _numberOfAttribute);
+  setAttributesDescriptions(s);
 
-//   nGr = IOR_Family->getNumberOfGroups();
-//   setNumberOfGroups(nGr);
+  nGr = IOR_Family->getNumberOfGroups();
+  setNumberOfGroups(nGr);
 
-//   convertCorbaArray(s, n, IOR_Family->getGroupsNames());
-//   ASSERT(n == _numberOfAttribute);
-//   setGroupsNames(s);
+  convertCorbaArray(s, n, IOR_Family->getGroupsNames());
+  ASSERT(n == _numberOfGroup);
+  setGroupsNames(s);
 
   _complete = false;
 
