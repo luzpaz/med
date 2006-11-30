dcd104154f8e99f98db87b68068618c9edd4e48f
jfa <jfa@opencascade.com>
2006-11-30 10:41:50 +0000

    Complete test for AsciiFieldDriver

--- src/MEDMEM/Test/MEDMEMTest_AsciiFieldDriver.cxx
+++ src/MEDMEM/Test/MEDMEMTest_AsciiFieldDriver.cxx
@@ -45,32 +45,35 @@ using namespace MEDMEM;
 /*!
  *  Check methods (8), defined in MEDMEM_AsciiFieldDriver.hxx:
  *
- *  (yetno) template<int N,unsigned int CODE> void fill(double *a, const double *b)
- *  (yetno) template<int N> bool compare(const double* a, const double* b)
- *  (yetno) template<> void fill<-1,0x3>(double *a, const double *b);
- *  (yetno) template<> bool compare<-1>(const double *a, const double *b);
+ *  (+)     template<int N,unsigned int CODE> void fill(double *a, const double *b)
+ *  (+)     template<int N> bool compare(const double* a, const double* b)
+ *  (+)     template<> void fill<-1,0x3>(double *a, const double *b);
+ *  (+)     template<> bool compare<-1>(const double *a, const double *b);
  *
  *  template <class T, int SPACEDIMENSION, unsigned int SORTSTRATEGY>
  *  class SDForSorting {
- *   (+) SDForSorting(const double *coords, const T* comp, int nbComponents);
- *   (+) SDForSorting(const SDForSorting& other);
- *   (+) ~SDForSorting();
- *   (+) bool operator< (const SDForSorting<T,SPACEDIMENSION,SORTSTRATEGY>& other) const;
- *   (+) void writeLine(ofstream& file) const;
+ *   (+)     SDForSorting(const double *coords, const T* comp, int nbComponents);
+ *   (+)     SDForSorting(const SDForSorting& other);
+ *   (+)     ~SDForSorting();
+ *   (+)     bool operator< (const SDForSorting<T,SPACEDIMENSION,SORTSTRATEGY>& other) const;
+ *   (+)     void writeLine(ofstream& file) const;
  *  }
  *
  *  template <class T>
  *  class ASCII_FIELD_DRIVER : public GENDRIVER {
- *   (yetno) template <class INTERLACING_TAG> ASCII_FIELD_DRIVER();
+ *
+ *           //MUST BE PRIVATE as there is no possibility to set _ptrField after this constructor usage
+ *   (-)     template <class INTERLACING_TAG> ASCII_FIELD_DRIVER();
+ *
  *   (+)     template <class INTERLACING_TAG>
  *           ASCII_FIELD_DRIVER(const string & fileName, FIELD<T,INTERLACING_TAG> * ptrField,
  *                              MED_EN::med_sort_direc direction=MED_EN::ASCENDING, const char *priority="");
- *   (+) ASCII_FIELD_DRIVER(const ASCII_FIELD_DRIVER<T>& other);
+ *   (+)     ASCII_FIELD_DRIVER(const ASCII_FIELD_DRIVER<T>& other);
  *   (+)     void open() throw (MEDEXCEPTION);
  *   (+)     void close();
  *   (+)     void read (void) throw (MEDEXCEPTION);
  *   (+)     void write(void) const throw (MEDEXCEPTION);
- *   (+) GENDRIVER* copy() const;
+ *   (+)     GENDRIVER* copy() const;
  *  }
  */
 void MEDMEMTest::testAsciiFieldDriver()
@@ -87,27 +90,99 @@ void MEDMEMTest::testAsciiFieldDriver()
   string anyfile1  = tmp_dir + "/anyfile1";
   string SDFfilename = tmp_dir + "/myfile";
   ofstream aFile(SDFfilename.c_str());
-  
+
   // To remove tmp files from disk
   MEDMEMTest_TmpFilesRemover aRemover;
   aRemover.Register(anyfile1);
   aRemover.Register(SDFfilename);
-  
+
   //Test SDForSorting class
-   double coord_1[10] = { 1.0, 2.0, 
-			 -1.0, 2.0, 
-			 3.6, -8.7, 
-			 10.0, -10.0,
-			 12.3, 9.3};
-  
-  int comp_1[5] = {1, 3, 5, 7, 9};
-  SDForSorting<int, 2, 48> aSDF_1(coord_1, comp_1, 5);
-  
-  SDForSorting<int, 2, 48> aSDFCpy_1 = SDForSorting<int, 2, 48>(aSDF_1);
-  CPPUNIT_ASSERT_EQUAL(aSDFCpy_1 < aSDF_1, false);
-  CPPUNIT_ASSERT_NO_THROW(aSDF_1.writeLine(aFile));
-  
-  
+  {
+    double coord_1[10] = { 1.0, 2.0,
+                           -1.0, 2.0,
+                           3.6, -8.7,
+                           10.0, -10.0,
+                           12.3, 9.3};
+
+    int comp_1[5] = {1, 3, 5, 7, 9};
+    SDForSorting<int, 2, 48> aSDF_1(coord_1, comp_1, 5);
+
+    SDForSorting<int, 2, 48> aSDFCpy_1 = SDForSorting<int, 2, 48>(aSDF_1);
+    CPPUNIT_ASSERT_EQUAL(aSDFCpy_1 < aSDF_1, false);
+    CPPUNIT_ASSERT_NO_THROW(aSDF_1.writeLine(aFile));
+  }
+
+  // Why functions
+  // template<> void MEDMEM::fill<-1,0x3>(double *a, const double *b)
+  // and
+  // template<> bool MEDMEM::compare<-1>(const double *a, const double *b)
+  // declared in MEDMEM_AsciiFieldDriver.hxx,
+  // are implemented in MEDMEM_DriverFactory.cxx?
+
+  // template<int N,unsigned int CODE> void fill(double *a, const double *b)
+  {
+    // 0x3 = 110000
+    double aa[3];
+    double bb[3] = {1,2,3};
+
+    fill<2,198>(aa, bb); // ZYX // 11000110 // 012
+    //MEDMEMTest_DumpArray<double>(cout, aa, 3, "aa filled with ZYX");
+    CPPUNIT_ASSERT_EQUAL(aa[0], bb[0]);
+    CPPUNIT_ASSERT_EQUAL(aa[1], bb[1]);
+    CPPUNIT_ASSERT_EQUAL(aa[2], bb[2]);
+
+    fill<2,210>(aa, bb); // ZXY // 11010010 // 102
+    //MEDMEMTest_DumpArray<double>(cout, aa, 3, "aa filled with ZXY");
+    CPPUNIT_ASSERT_EQUAL(aa[0], bb[1]);
+    CPPUNIT_ASSERT_EQUAL(aa[1], bb[0]);
+    CPPUNIT_ASSERT_EQUAL(aa[2], bb[2]);
+
+    fill<2,228>(aa, bb); // XYZ // 11100100 // 210
+    //MEDMEMTest_DumpArray<double>(cout, aa, 3, "aa filled with XYZ");
+    CPPUNIT_ASSERT_EQUAL(aa[0], bb[2]);
+    CPPUNIT_ASSERT_EQUAL(aa[1], bb[1]);
+    CPPUNIT_ASSERT_EQUAL(aa[2], bb[0]);
+  }
+
+  // template<int N> bool compare(const double* a, const double* b)
+  {
+    {
+      // aa < bb
+      double aa[8] = {1,1,1,1,1,1,1,1};
+      double bb[8] = {1,1,1,1,1,1,1,2};
+      CPPUNIT_ASSERT(compare<7>(aa, bb));
+    }
+
+    {
+      // aa < bb
+      double aa[8] = {1,1,1,1,1,1,1,1};
+      double bb[8] = {2,1,1,1,1,1,1,1};
+      CPPUNIT_ASSERT(compare<7>(aa, bb));
+    }
+
+    {
+      // aa > bb
+      double aa[8] = {2,1,1,1,1,1,1,1};
+      double bb[8] = {1,1,1,1,1,1,1,1};
+      CPPUNIT_ASSERT(!compare<7>(aa, bb));
+    }
+
+    {
+      // aa == bb
+      double aa[8] = {1,1,1,1,1,1,1,1};
+      double bb[8] = {1,1,1,1,1,1,1,1};
+      CPPUNIT_ASSERT(!compare<7>(aa, bb));
+    }
+
+    {
+      // compare<-1>
+      double aa[8] = {2,1,1,1,1,1,1,1};
+      double bb[8] = {1,1,1,1,1,1,1,1};
+      CPPUNIT_ASSERT(!compare<-1>(aa, bb));
+    }
+  }
+
+  // Test ASCII_FIELD_DRIVER
   FIELD<double> * aField1 = new FIELD<double> (MED_DRIVER, filename, fieldname);
   const SUPPORT * aSupport = aField1->getSupport();
   MESH * aMesh = new MESH (MED_DRIVER, filename, aSupport->getMeshName());
@@ -122,7 +197,7 @@ void MEDMEMTest::testAsciiFieldDriver()
   CPPUNIT_ASSERT(aDriver1->getAccessMode() == MED_EN::MED_ECRI);
 
   // and write the field on disk
-  
+
   // must throw because the file is not opened
 #ifdef ENABLE_FORCED_FAILURES
   // (BUG) Invalid opened/closed state management
@@ -196,19 +271,16 @@ void MEDMEMTest::testAsciiFieldDriver()
 
   //Copy constructor
   ASCII_FIELD_DRIVER<double> aDriver1_Cpy1 = ASCII_FIELD_DRIVER<double> (*aDriver1);
-  
+
   //Test copy() function
   ASCII_FIELD_DRIVER<double> *aDriver1_Cpy2 = (ASCII_FIELD_DRIVER<double>*)aDriver1->copy();
-  
+
   //Compare objects
-#ifdef ENABLE_FORCED_FAILURES  
+#ifdef ENABLE_FORCED_FAILURES
   CPPUNIT_ASSERT_EQUAL(aDriver1_Cpy1, *aDriver1);
   CPPUNIT_ASSERT_EQUAL(*aDriver1_Cpy2, *aDriver1);
 #endif
-  
 
-  
-  
   // free memory
   delete aDriver1;
   delete aField1;
