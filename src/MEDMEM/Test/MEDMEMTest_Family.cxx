e336da98c687b02f61c071dcbfe47c826b13543c
jfa <jfa@opencascade.com>
2006-12-05 13:25:57 +0000

    Complete test for Family.

--- src/MEDMEM/Test/MEDMEMTest_Family.cxx
+++ src/MEDMEM/Test/MEDMEMTest_Family.cxx
@@ -26,6 +26,7 @@
 #include "MEDMEM_MedMeshDriver.hxx"
 #include "MEDMEM_Mesh.hxx"
 #include "MEDMEM_Support.hxx"
+#include "MEDMEM_Compatibility21_22.hxx"
 
 #include <sstream>
 #include <cmath>
@@ -45,9 +46,7 @@ using namespace MEDMEM;
  *  Check methods (24), defined in MEDMEM_Family.hxx:
  *  class FAMILY : virtual public SUPPORT {
  *   (+)     FAMILY();
- *
- *           // Constructor to use with med driver.
- *   (!)     FAMILY(MESH* Mesh, int Identifier, string Name,
+ *   (+)     FAMILY(MESH* Mesh, int Identifier, string Name,
  *                  int NumberOfAttribute, int *AttributeIdentifier,
  *                  int *AttributeValue, string AttributeDescription,
  *                  int NumberOfGroup,   string GroupName,
@@ -63,8 +62,7 @@ using namespace MEDMEM;
  *   (+)     friend ostream & operator<<(ostream &os, FAMILY &my);
  *   (+)     friend ostream & operator<<(ostream &os, const FAMILY &my);
  *
- *           // Is used only in constructor. Why it is public?
- *   (!)     bool build(MED_EN::medEntityMesh Entity,int **FamilyNumber);
+ *   (+)     bool build(MED_EN::medEntityMesh Entity,int **FamilyNumber);
  *
  *   (+)     inline void setIdentifier             (int Identifier);
  *   (+)     inline void setNumberOfAttributes     (int NumberOfAttribute);
@@ -188,6 +186,7 @@ void MEDMEMTest::testFamily()
                 aName, aDescr, anEntity, aNbGeomTypes,
                 id, nbAttributes, nbGroups);
 
+  // Copy constructor
   FAMILY * aFamily2 = new FAMILY (* aFamily1);
 
   // Dump
@@ -203,6 +202,7 @@ void MEDMEMTest::testFamily()
                 aName, aDescr, anEntity, aNbGeomTypes,
                 id, nbAttributes, nbGroups);
 
+  // One more copy
   FAMILY * aFamily3 = new FAMILY (* aFamily2);
   delete aFamily2;
 
@@ -221,93 +221,105 @@ void MEDMEMTest::testFamily()
   ////////////
   // TEST 2 //
   ////////////
+  {
+    // Prepare data
 
-  // Change aFamily3
+    // attributes
+    int   newNumberOfAttribute = 3;
+    int * newAttributeIdentifier = new int[newNumberOfAttribute];
+    int * newAttributeValue = new int[newNumberOfAttribute];
+    string * newAttributeDescription = new string[newNumberOfAttribute];
 
-  // attributes
-  int   newNumberOfAttribute = 3;
-  int * newAttributeIdentifier = new int[newNumberOfAttribute];
-  int * newAttributeValue = new int[newNumberOfAttribute];
-  string * newAttributeDescription = new string[newNumberOfAttribute];
+    char tmp [32];
+    for (int i = 0; i < newNumberOfAttribute; i++) {
+      newAttributeIdentifier[i] = i+1;
+      newAttributeValue[i] = (i+1)*10;
+      sprintf(tmp, "Attribut N° %d", i+1);
+      newAttributeDescription[i] = tmp;
+    }
 
-  char tmp [32];
-  for (int i = 0; i < newNumberOfAttribute; i++) {
-    newAttributeIdentifier[i] = i+1;
-    newAttributeValue[i] = (i+1)*10;
-    sprintf(tmp, "Attribut N° %d", i+1);
-    newAttributeDescription[i] = tmp;
-  }
+    // groups
+    int newNumberOfGroups = 4;
+    string * newGroups = new string[newNumberOfGroups];
+    for (int i = 0; i < newNumberOfGroups; i++) {
+      sprintf(tmp, "Group N° %d", i+1);
+      newGroups[i] = tmp;
+    }
 
-  aFamily3->setNumberOfAttributes(newNumberOfAttribute);
-  aFamily3->setAttributesIdentifiers(newAttributeIdentifier);
-  aFamily3->setAttributesValues(newAttributeValue);
-  aFamily3->setAttributesDescriptions(newAttributeDescription);
+    // Change aFamily3
 
-  // groups
-  int newNumberOfGroups = 4;
-  string * newGroups = new string[newNumberOfGroups];
-  for (int i = 0; i < newNumberOfGroups; i++) {
-    sprintf(tmp, "Group N° %d", i+1);
-    newGroups[i] = tmp;
-  }
+    // attributes
+    aFamily3->setNumberOfAttributes(newNumberOfAttribute);
+    aFamily3->setAttributesIdentifiers(newAttributeIdentifier);
+    aFamily3->setAttributesValues(newAttributeValue);
+    aFamily3->setAttributesDescriptions(newAttributeDescription);
 
-  aFamily3->setNumberOfGroups(newNumberOfGroups);
-  aFamily3->setGroupsNames(newGroups, /*giveOwnership = */true);
+    // groups
+    aFamily3->setNumberOfGroups(newNumberOfGroups);
+    aFamily3->setGroupsNames(newGroups, /*giveOwnership = */true);
 
-  // identifier
-  aFamily3->setIdentifier(77);
+    // identifier
+    aFamily3->setIdentifier(77);
 
-  // check new parameters of aFamily3
+    // check new parameters of aFamily3
 
-  // attributes
-  CPPUNIT_ASSERT_EQUAL(newNumberOfAttribute, aFamily3->getNumberOfAttributes());
-  const int *    newAttributeIdentifierBack  = aFamily3->getAttributesIdentifiers();
-  const int *    newAttributeValueBack       = aFamily3->getAttributesValues();
-  const string * newAttributeDescriptionBack = aFamily3->getAttributesDescriptions();
-  for (int i = 1; i <= newNumberOfAttribute; i++) {
-    sprintf(tmp, "Attribut N° %d", i);
-    string descr (tmp);
+    // attributes
+    CPPUNIT_ASSERT_EQUAL(newNumberOfAttribute, aFamily3->getNumberOfAttributes());
+    const int *    newAttributeIdentifierBack  = aFamily3->getAttributesIdentifiers();
+    const int *    newAttributeValueBack       = aFamily3->getAttributesValues();
+    const string * newAttributeDescriptionBack = aFamily3->getAttributesDescriptions();
+    for (int i = 1; i <= newNumberOfAttribute; i++) {
+      sprintf(tmp, "Attribut N° %d", i);
+      string descr (tmp);
 
-    CPPUNIT_ASSERT_EQUAL(i    , aFamily3->getAttributeIdentifier(i));
-    CPPUNIT_ASSERT_EQUAL(i*10 , aFamily3->getAttributeValue(i));
-    CPPUNIT_ASSERT_EQUAL(descr, aFamily3->getAttributeDescription(i));
+      CPPUNIT_ASSERT_EQUAL(i    , aFamily3->getAttributeIdentifier(i));
+      CPPUNIT_ASSERT_EQUAL(i*10 , aFamily3->getAttributeValue(i));
+      CPPUNIT_ASSERT_EQUAL(descr, aFamily3->getAttributeDescription(i));
 
-    CPPUNIT_ASSERT_EQUAL(i    , newAttributeIdentifierBack[i - 1]);
-    CPPUNIT_ASSERT_EQUAL(i*10 , newAttributeValueBack[i - 1]);
-    CPPUNIT_ASSERT_EQUAL(descr, newAttributeDescriptionBack[i - 1]);
-  }
+      CPPUNIT_ASSERT_EQUAL(i    , newAttributeIdentifierBack[i - 1]);
+      CPPUNIT_ASSERT_EQUAL(i*10 , newAttributeValueBack[i - 1]);
+      CPPUNIT_ASSERT_EQUAL(descr, newAttributeDescriptionBack[i - 1]);
+    }
 
-  // checked invalid indices
+    delete [] newAttributeIdentifier;
+    delete [] newAttributeValue;
+    delete [] newAttributeDescription;
+
+    // check invalid indices
 #ifdef ENABLE_FORCED_FAILURES
-  // (BUG) Index is not checked to be in valid range
-  CPPUNIT_ASSERT_THROW(aFamily3->getAttributeIdentifier(newNumberOfAttribute + 1), MEDEXCEPTION);
-  CPPUNIT_ASSERT_THROW(aFamily3->getAttributeDescription(0), MEDEXCEPTION);
+    // (BUG) Index is not checked to be in valid range
+    CPPUNIT_ASSERT_THROW(aFamily3->getAttributeIdentifier(newNumberOfAttribute + 1), MEDEXCEPTION);
+    CPPUNIT_ASSERT_THROW(aFamily3->getAttributeDescription(0), MEDEXCEPTION);
 #endif
 
-  // groups
-  CPPUNIT_ASSERT_EQUAL(newNumberOfGroups, aFamily3->getNumberOfGroups());
-  const string * newGroupsBack = aFamily3->getGroupsNames();
-  for (int i = 1; i <= newNumberOfGroups; i++) {
-    sprintf(tmp, "Group N° %d", i);
-    string grp (tmp);
+    // groups
+    CPPUNIT_ASSERT_EQUAL(newNumberOfGroups, aFamily3->getNumberOfGroups());
+    const string * newGroupsBack = aFamily3->getGroupsNames();
+    for (int i = 1; i <= newNumberOfGroups; i++) {
+      sprintf(tmp, "Group N° %d", i);
+      string grp (tmp);
 
-    CPPUNIT_ASSERT_EQUAL(grp, aFamily3->getGroupName(i));
-    CPPUNIT_ASSERT_EQUAL(grp, newGroupsBack[i - 1]);
+      CPPUNIT_ASSERT_EQUAL(grp, aFamily3->getGroupName(i));
+      CPPUNIT_ASSERT_EQUAL(grp, newGroupsBack[i - 1]);
+    }
+
+    // identifier
+    CPPUNIT_ASSERT_EQUAL(77, aFamily3->getIdentifier());
   }
 
-  // identifier
-  CPPUNIT_ASSERT_EQUAL(77, aFamily3->getIdentifier());
-
-  // check default constructor and operator=
-  FAMILY aFamily4;
+  /////////////////////////////////////////////////////
+  // TEST 3: check default constructor and operator= //
+  /////////////////////////////////////////////////////
+  {
+    FAMILY aFamily4;
 #ifdef ENABLE_FAULTS
-  aFamily4 = (const FAMILY &)*aFamily3;
+    aFamily4 = (const FAMILY &)*aFamily3;
 #endif
 #ifdef ENABLE_FORCED_FAILURES
-  // (BUG) Wrong implementation or usage of PointerOf<string>.
-  //       Do not use memcpy() with array of std::string!
-  CPPUNIT_FAIL("Impossible to use FAMILY::operator= because of"
-               " wrong implementation or usage of PointerOf<string>");
+    // (BUG) Wrong implementation or usage of PointerOf<string>.
+    //       Do not use memcpy() with array of std::string!
+    CPPUNIT_FAIL("Impossible to use FAMILY::operator= because of"
+                 " wrong implementation or usage of PointerOf<string>");
   /*{
     int nb = 3;
     string * str = new string[nb];
@@ -343,27 +355,144 @@ void MEDMEMTest::testFamily()
   }
   */
 #endif
+  }
 
-  ////////////
-  // TEST 3 //
-  ////////////
-
-  // check construction from given support
-  SUPPORT s1 (*aFamily3);
-  FAMILY  f1 (s1);
-  CPPUNIT_ASSERT_EQUAL(0, f1.getIdentifier());
-  CPPUNIT_ASSERT_EQUAL(0, f1.getNumberOfAttributes());
-  CPPUNIT_ASSERT_EQUAL(0, f1.getNumberOfGroups());
-  CPPUNIT_ASSERT(s1.deepCompare(f1));
+  ///////////////////////////////////////////////////
+  // TEST 4: check construction from given support //
+  ///////////////////////////////////////////////////
+  {
+    SUPPORT s1 (*aFamily3);
+    FAMILY  f1 (s1);
+    CPPUNIT_ASSERT_EQUAL(0, f1.getIdentifier());
+    CPPUNIT_ASSERT_EQUAL(0, f1.getNumberOfAttributes());
+    CPPUNIT_ASSERT_EQUAL(0, f1.getNumberOfGroups());
+    CPPUNIT_ASSERT(s1.deepCompare(f1));
+  }
 
   delete aFamily3;
-
-  delete [] newAttributeIdentifier;
-  delete [] newAttributeValue;
-  delete [] newAttributeDescription;
-
   delete aMesh;
 
-  //CPPUNIT_FAIL("Constructor FAMILY::FAMILY(MESH*, int, string, etc.) and method build()"
-  //             " is not tested because it is designed to be used only with med driver");
+  /////////////////////////////////////////////////////////////////
+  // TEST 5: check constructor, designed to use with med driver. //
+  /////////////////////////////////////////////////////////////////
+  {
+    MESH * aTestMesh = MEDMEMTest_createTestMesh();
+
+    // TestMesh families. Let's imagine that we would like to have
+    // the following nodes and elements distribution by families:
+
+    // NumberOfNodes = 19;
+    int aNodeFamily[19] = {1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+                           2, 2, 2, 2, 2, 2, 3, 3, 3};
+
+    // NumberOfCellTypes = 3;
+    // NumberOfCells[NumberOfCellTypes] = {12, 2, 2};
+    int aCellType1Family[12] = {4,4,4,4,4,5,5,5,5,5,5,5};
+    int aCellType2Family[ 2] = {5,6};
+    int aCellType3Family[ 2] = {6,6};
+    int * aCellFamily[3] = {aCellType1Family, aCellType2Family, aCellType3Family};
+
+    // NumberOfFaceTypes = 2;
+    // NumberOfFaces[NumberOfFaceTypes] = {4, 4};
+    int aFaceType1Family[4] = {7,7,7,7};
+    int aFaceType2Family[4] = {7,7,7,7};
+    int * aFaceFamily[3] = {aFaceType1Family, aFaceType2Family};
+
+    // No edges in aTestMesh
+    int ** anEdgeFamily = NULL;
+
+    // new families data
+    int attrId[2] = {1,2};
+    int attrVa[2] = {7,8};
+
+    string attrDescr1 ("Attribute 1 description");
+    string attrDescr2 ("Attribute 2 description");
+    string attrDescrEmpty (MED_TAILLE_DESC - 23, ' ');
+    attrDescr1 += attrDescrEmpty;
+    attrDescr2 += attrDescrEmpty;
+    CPPUNIT_ASSERT(MED_TAILLE_DESC == attrDescr1.length());
+    CPPUNIT_ASSERT(MED_TAILLE_DESC == attrDescr2.length());
+    string attrDescr = attrDescr1 + attrDescr2;
+
+    string groupName1 ("Group_1");
+    string groupName2 ("Group_2");
+    string groupName3 ("Group_3");
+    string groupNameEmpty (MED_TAILLE_LNOM - 7, ' ');
+    groupName1 += groupNameEmpty;
+    groupName2 += groupNameEmpty;
+    groupName3 += groupNameEmpty;
+    CPPUNIT_ASSERT(MED_TAILLE_LNOM == groupName1.length());
+    CPPUNIT_ASSERT(MED_TAILLE_LNOM == groupName2.length());
+    CPPUNIT_ASSERT(MED_TAILLE_LNOM == groupName3.length());
+    string groupNames = groupName1 + groupName2 + groupName3;
+
+    // nodes family 1
+    FAMILY aNodesF1 (aTestMesh, /*Identifier*/1, "Nodes 1", 
+                     /*NumberOfAttribute*/2, attrId, attrVa, attrDescr,
+                     /*NumberOfGroup*/3, groupNames,
+                     aNodeFamily, aCellFamily, aFaceFamily, anEdgeFamily);
+
+    //cout << "Show aNodesF1 :" << endl;
+    //cout << aNodesF1 << endl;
+
+    CPPUNIT_ASSERT_EQUAL(1, aNodesF1.getIdentifier());
+    CPPUNIT_ASSERT(strcmp("Nodes 1", aNodesF1.getName().c_str()) == 0);
+    CPPUNIT_ASSERT(MED_EN::MED_NODE == aNodesF1.getEntity());
+    CPPUNIT_ASSERT(!aNodesF1.isOnAllElements());
+    CPPUNIT_ASSERT_EQUAL(7, aNodesF1.getNumberOfElements(MED_EN::MED_ALL_ELEMENTS));
+
+    CPPUNIT_ASSERT_EQUAL(2, aNodesF1.getNumberOfAttributes());
+    CPPUNIT_ASSERT_EQUAL(1, aNodesF1.getAttributeIdentifier(1));
+    CPPUNIT_ASSERT_EQUAL(2, aNodesF1.getAttributeIdentifier(2));
+    CPPUNIT_ASSERT_EQUAL(7, aNodesF1.getAttributeValue(1));
+    CPPUNIT_ASSERT_EQUAL(8, aNodesF1.getAttributeValue(2));
+    CPPUNIT_ASSERT_EQUAL(attrDescr1, aNodesF1.getAttributeDescription(1));
+    CPPUNIT_ASSERT_EQUAL(attrDescr2, aNodesF1.getAttributeDescription(2));
+
+    CPPUNIT_ASSERT_EQUAL(3, aNodesF1.getNumberOfGroups());
+    CPPUNIT_ASSERT_EQUAL(groupName1, aNodesF1.getGroupName(1));
+    CPPUNIT_ASSERT_EQUAL(groupName2, aNodesF1.getGroupName(2));
+    CPPUNIT_ASSERT_EQUAL(groupName3, aNodesF1.getGroupName(3));
+
+    // faces family 7
+    FAMILY aFacesF7 (aTestMesh, /*Identifier*/7, "Faces All", 
+                     /*NumberOfAttribute*/2, attrId, attrVa, attrDescr,
+                     /*NumberOfGroup*/3, groupNames,
+                     aNodeFamily, aCellFamily, aFaceFamily, anEdgeFamily);
+
+    cout << "Show aFacesF7 :" << endl;
+    cout << aFacesF7 << endl;
+
+    CPPUNIT_ASSERT_EQUAL(7, aFacesF7.getIdentifier());
+    CPPUNIT_ASSERT(strcmp("Faces All", aFacesF7.getName().c_str()) == 0);
+    CPPUNIT_ASSERT(MED_EN::MED_FACE == aFacesF7.getEntity());
+
+    CPPUNIT_ASSERT_EQUAL(8, aTestMesh->getNumberOfElementsWithPoly(MED_EN::MED_FACE,
+                                                                   MED_EN::MED_ALL_ELEMENTS));
+    CPPUNIT_ASSERT_EQUAL(8, aFacesF7.getNumberOfElements(MED_EN::MED_ALL_ELEMENTS));
+
+#ifdef ENABLE_FORCED_FAILURES
+    // ? (BUG) ? Why _isOnAllElts is set to true only for nodes and cells. What about faces?
+    //      _isOnAllElts = true ;
+    // See MEDMEM_Family.cxx lines 118-119 and 386-387.
+    CPPUNIT_ASSERT(aFacesF7.isOnAllElements());
+#endif
+
+    CPPUNIT_ASSERT_EQUAL(2, aFacesF7.getNumberOfAttributes());
+    CPPUNIT_ASSERT_EQUAL(1, aFacesF7.getAttributeIdentifier(1));
+    CPPUNIT_ASSERT_EQUAL(2, aFacesF7.getAttributeIdentifier(2));
+    CPPUNIT_ASSERT_EQUAL(7, aFacesF7.getAttributeValue(1));
+    CPPUNIT_ASSERT_EQUAL(8, aFacesF7.getAttributeValue(2));
+    CPPUNIT_ASSERT_EQUAL(attrDescr1, aFacesF7.getAttributeDescription(1));
+    CPPUNIT_ASSERT_EQUAL(attrDescr2, aFacesF7.getAttributeDescription(2));
+
+    CPPUNIT_ASSERT_EQUAL(3, aFacesF7.getNumberOfGroups());
+    CPPUNIT_ASSERT_EQUAL(groupName1, aFacesF7.getGroupName(1));
+    CPPUNIT_ASSERT_EQUAL(groupName2, aFacesF7.getGroupName(2));
+    CPPUNIT_ASSERT_EQUAL(groupName3, aFacesF7.getGroupName(3));
+
+    delete aTestMesh;
+
+    // Method build() is not tested directly, but it is called from constructor, tested here
+  }
 }
