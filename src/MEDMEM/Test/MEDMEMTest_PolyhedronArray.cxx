ac80af19514e654c21db0284c5161975638e292b
jfa <jfa@opencascade.com>
2007-02-14 13:13:10 +0000

    Join modifications from branch BR_MEDMEM_Unit_Tests

---
+++ src/MEDMEM/Test/MEDMEMTest_PolyhedronArray.cxx
@@ -0,0 +1,103 @@
+// Copyright (C) 2006  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+// CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+
+#include "MEDMEMTest.hxx"
+#include <cppunit/Message.h>
+#include <cppunit/TestAssert.h>
+
+#include "MEDMEM_PolyhedronArray.hxx"
+#include "MEDMEM_define.hxx"
+
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+using namespace MEDMEM;
+using namespace MED_EN;
+
+/*!
+ *  Check methods (12), defined in MEDMEM_PolyhedronArray.hxx:
+ *  class POLYHEDRONARRAY {
+ *   (+) POLYHEDRONARRAY();
+ *   (+) POLYHEDRONARRAY(MED_EN::med_int numberofpolyhedron,
+ *                       MED_EN::med_int numberoffaces, MED_EN::med_int numberofnodes);
+ *   (+) POLYHEDRONARRAY(const POLYHEDRONARRAY& m);
+ *
+ *   (+) inline MED_EN::med_int getNumberOfPolyhedron() const;
+ *   (+) inline MED_EN::med_int getNumberOfFaces() const;
+ *   (+) inline MED_EN::med_int getNumberOfNodes() const;
+ *
+ *   (+) inline const MED_EN::med_int* getPolyhedronIndex() const;
+ *   (+) inline const MED_EN::med_int* getFacesIndex() const;
+ *   (+) inline const MED_EN::med_int* getNodes() const;
+ *
+ *   (+) inline void setPolyhedronIndex(const MED_EN::med_int* polyhedronindex);
+ *   (+) inline void setFacesIndex(const MED_EN::med_int* facesindex);
+ *   (+) inline void setNodes(const MED_EN::med_int* nodes);
+ *  }
+ */
+void MEDMEMTest::testPolyhedronArray()
+{
+  const med_int nbPolyhedrons = 2;
+  const med_int nbFaces = 11;
+  const med_int nbNodes = 40;
+
+  med_int polyhedronindex[nbPolyhedrons] = {1,7};
+  med_int facesindex[nbFaces] = {1,5,9,13,17,21,25,28,31,34,37};
+  med_int nodes[nbNodes] = {1,2,6,5,2,3,7,6,4,3,7,8,1,4,8,5,1,2,3,4,5,6,7,8,5,6,9,6,7,9,7,8,9,8,5,9,5,6,7,8};
+
+  // construction with parameters
+  POLYHEDRONARRAY myPArray (nbPolyhedrons, nbFaces, nbNodes);
+
+  CPPUNIT_ASSERT_NO_THROW(myPArray.setPolyhedronIndex(polyhedronindex));
+  CPPUNIT_ASSERT_NO_THROW(myPArray.setFacesIndex(facesindex));
+  CPPUNIT_ASSERT_NO_THROW(myPArray.setNodes(nodes));
+
+  // copy constructor
+  POLYHEDRONARRAY myPArray2 (myPArray);
+
+  const med_int* __polyhedronindex = myPArray2.getPolyhedronIndex();
+  const med_int* __facesindex = myPArray2.getFacesIndex();
+  const med_int* __nodes = myPArray2.getNodes();
+
+  CPPUNIT_ASSERT_EQUAL(myPArray2.getNumberOfPolyhedron(), nbPolyhedrons);
+  CPPUNIT_ASSERT_EQUAL(myPArray2.getNumberOfFaces(), nbFaces);
+  CPPUNIT_ASSERT_EQUAL(myPArray2.getNumberOfNodes(), nbNodes);
+
+  for (int i = 0; i < nbPolyhedrons; i++)
+    CPPUNIT_ASSERT_EQUAL(__polyhedronindex[i], polyhedronindex[i]);
+
+  for (int i = 0; i < nbFaces; i++)
+   CPPUNIT_ASSERT_EQUAL(__facesindex[i], facesindex[i]);
+
+  for (int i = 0; i < nbNodes; i++)
+    CPPUNIT_ASSERT_EQUAL(__nodes[i], nodes[i]);
+
+  // default constructor (has no sense, must be private)
+  POLYHEDRONARRAY myPArray3;
+
+  CPPUNIT_ASSERT_EQUAL(0, myPArray3.getNumberOfPolyhedron());
+  CPPUNIT_ASSERT_EQUAL(0, myPArray3.getNumberOfFaces());
+  CPPUNIT_ASSERT_EQUAL(0, myPArray3.getNumberOfNodes());
+
+  CPPUNIT_ASSERT(myPArray3.getPolyhedronIndex() == NULL);
+  CPPUNIT_ASSERT(myPArray3.getFacesIndex() == NULL);
+  CPPUNIT_ASSERT(myPArray3.getNodes() == NULL);
+}
---
+++ src/MEDMEM/Test/MEDMEMTest_Unit.cxx
@@ -0,0 +1,353 @@
+// Copyright (C) 2006  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+// CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+
+#include "MEDMEMTest.hxx"
+#include <cppunit/Message.h>
+#include <cppunit/TestAssert.h>
+
+#include <cstdlib>
+#include <exception>
+
+#include <MEDMEM_Unit.hxx>
+
+using namespace std;
+using namespace MEDMEM;
+
+
+/*!
+ *  Check methods (18), defined in MEDMEM_Unit.hxx:
+ * class UNIT {
+ *   (+)     UNIT();
+ *   (+)     UNIT(string Name, string Description);
+ *   (+)     ~UNIT();
+ *   (+)     UNIT & operator=(const UNIT &unit);
+ *   (+)     inline void setName(string Name);
+ *   (+)     inline void setDescription(string Description);
+ *   (+)     inline void setMasse(int Masse);
+ *   (+)     inline void setLength(int Length);
+ *   (+)     inline void setTime(int Time);
+ *   (+)     inline void setTemperature(int Temperature);
+ *   (+)     inline void setMatterQuantity(int MatterQuantity);
+ *   (+)     void setCurrentStrength(int CurrentStrength);
+ *   (+)     inline void setLightIntensity(int LightIntensity);
+ *
+ *   (+)     inline string getName() const;
+ *   (+)     inline string getDescription() const;
+ *   (+)     inline int    getMasse() const;
+ *   (+)     inline int    getLength() const;
+ *   (+)     inline int    getTime() const;
+ *   (+)     inline int    getTemperature() const;
+ *   (+)     inline int    getMatterQuantity() const;
+ *   (+)     inline int    getCurrentStrength() const;
+ *   (+)     inline int    getLightIntensity() const;
+ * }
+ *
+ * Used code of MEDMEM/tests/testUUnit.cxx
+ */
+
+void MEDMEMTest::testUnit()
+{
+  string myName        = "NameForTesting";
+  string myDescription = "all values are setted 10";
+  int    myMasse           = 10;
+  int    myLength          = 10;
+  int    myTime            = 10;
+  int    myTemperature     = 10;
+  int    myMatterQuantity  = 10;
+  int    myCurrentStrength = 10;
+  int    myLightIntensity  = 10;
+
+  //Default constructor
+  UNIT   myUNIT;
+
+  //Normal constructor
+  UNIT *myUNIT1 = new UNIT(myName,myDescription);
+  CPPUNIT_ASSERT(myUNIT1);
+
+  //--------------------------------------------------------------------//
+  //                        Tests des methodes                          //
+  //                                                                    //
+  // - setName                                                          //
+  // - setDescription                                                   //
+  // - setMasse                                                         //
+  // - setLength                                                        //
+  // - setTime                                                          //
+  // - setTemperature                                                   //
+  // - setMatterQuantity                                                //
+  // - setCurrentStrength                                               //
+  // - setLightIntensity                                                //
+  //--------------------------------------------------------------------//
+
+  try
+  {
+    myUNIT.setName(myName);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setDescription(myDescription);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("MEDMEM_UNIT::setDescription(): Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setMasse(myMasse);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setLength(myLength);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setTime(myTime);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setTemperature(myTemperature);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setMatterQuantity(myMatterQuantity);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setCurrentStrength(myCurrentStrength);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    myUNIT.setLightIntensity(myLightIntensity);
+  }
+  catch(const std::exception &e)
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch(...)
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  //--------------------------------------------------------------------//
+  //                        Tests des methodes                          //
+  //                                                                    //
+  // - getName                                                          //
+  // - getDescription                                                   //
+  // - getMasse                                                         //
+  // - getLength                                                        //
+  // - getTime                                                          //
+  // - getTemperature                                                   //
+  // - getMatterQuantity                                                //
+  // - getCurrentStrength                                               //
+  // - getLightIntensity                                                //
+  //--------------------------------------------------------------------//
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getName() == myName );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getDescription() == myDescription );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getMasse() == myMasse );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getLength() == myLength );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+    try
+  {
+    CPPUNIT_ASSERT( myUNIT.getTime() == myTime );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getTemperature() == myTemperature );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getMatterQuantity() == myMatterQuantity );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getCurrentStrength() == myCurrentStrength );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  try
+  {
+    CPPUNIT_ASSERT( myUNIT.getLightIntensity() == myLightIntensity );
+  }
+  catch( const std::exception &e )
+  {
+    CPPUNIT_FAIL(e.what());
+  }
+  catch ( ... )
+  {
+    CPPUNIT_FAIL("Unknown exception");
+  }
+
+  //--------------------------------------------------------------------//
+  //                        Tests des method                            //
+  // - operator=                                                        //
+  //--------------------------------------------------------------------//
+
+  UNIT myUNITCpy;
+  myUNITCpy = myUNIT;
+  CPPUNIT_ASSERT_EQUAL( myUNITCpy.getTemperature(), myUNITCpy.getTemperature());
+}
