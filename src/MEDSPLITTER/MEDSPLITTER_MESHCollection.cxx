14fb623c290f3b9edd05801868715a54c7508e1f
jfa <jfa@opencascade.com>
2007-05-31 08:15:14 +0000

    Restore right code (roll back temporary fix for MULTIPR).

--- src/MEDSPLITTER/MEDSPLITTER_MESHCollection.cxx
+++ src/MEDSPLITTER/MEDSPLITTER_MESHCollection.cxx
@@ -78,10 +78,9 @@ MESHCollection::MESHCollection(const MESHCollection& initial_collection, Topolog
 			//creating the new mesh
 			MEDMEM::MESHING* mesh_builder=new MEDMEM::MESHING;
 			m_mesh[idomain]= static_cast<MEDMEM::MESH*> (mesh_builder);
-			//ostringstream osname;
-			//osname << mesh_name<<"_"<<idomain+1;
-			//mesh_builder->setName(osname.str());
-			mesh_builder->setName(mesh_name);
+			ostringstream osname;
+			osname << mesh_name<<"_"<<idomain+1;
+			mesh_builder->setName(osname.str());
 
 			createNodalConnectivity(initial_collection,idomain, MED_EN::MED_CELL);
 			mesh_builder->setMeshDimension(initial_collection.getMeshDimension());
