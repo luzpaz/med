ed2df542b2a9b1d6766c039c212ff9067337ec38
eap <eap@opencascade.com>
2008-05-21 13:28:53 +0000

    PAL19796 MED Unit test on Mandriva2008        do not compare mesh type against MED_STRUCTURE which does not exist in med-2.1

--- src/MEDMEM/MEDMEM_MedMeshDriver21.cxx
+++ src/MEDMEM/MEDMEM_MedMeshDriver21.cxx
@@ -175,27 +175,27 @@ void MED_MESH_RDONLY_DRIVER21::read(void)
       END_OF(LOC);
       return;
     }
-  else // check that the mesh is really unstructured (PAL14113)
-  {
-    char                  meshName[MED_TAILLE_NOM+1]="";
-    char                  meshDescription[MED_TAILLE_DESC+1]="";
-    med_2_3::med_int      meshDim;
-    med_2_3::med_maillage meshType;
-    int numberOfMeshes = med_2_3::MEDnMaa(_medIdt);
-    for (int i=1;i<=numberOfMeshes;i++)
-    {
-      MEDmaaInfo(_medIdt, i ,meshName, &meshDim, &meshType, meshDescription);
-      if (_meshName == string(meshName)) {
-        if ( meshType == med_2_3::MED_STRUCTURE ) {
-          throw MEDEXCEPTION(LOCALIZED(STRING(LOC) <<
-                                       "class GRID must be used for a structured mesh"));
-        }
-        else {
-          break;
-        }
-      }
-    }
-  }
+//   else // check that the mesh is really unstructured (PAL14113)
+//   {
+//     char                  meshName[MED_TAILLE_NOM+1]="";
+//     char                  meshDescription[MED_TAILLE_DESC+1]="";
+//     med_2_3::med_int      meshDim;
+//     med_2_3::med_maillage meshType;
+//     int numberOfMeshes = med_2_3::MEDnMaa(_medIdt);
+//     for (int i=1;i<=numberOfMeshes;i++)
+//     {
+//       MEDmaaInfo(_medIdt, i ,meshName, &meshDim, &meshType, meshDescription);
+//       if (_meshName == string(meshName)) {
+//         if ( meshType == med_2_3::MED_STRUCTURE ) {
+//           throw MEDEXCEPTION(LOCALIZED(STRING(LOC) <<
+//                                        "class GRID must be used for a structured mesh"));
+//         }
+//         else {
+//           break;
+//         }
+//       }
+//     }
+//   }
 
   if (getCOORDINATE()!=MED_VALID)
     throw MEDEXCEPTION(LOCALIZED(STRING(LOC) << "ERREUR in getCOORDINATE"  )) ;
