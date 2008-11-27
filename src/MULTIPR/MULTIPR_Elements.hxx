46a8455502be95ef42a53c035e14cd811b3b846a
abd <abd@opencascade.com>
2008-11-27 06:47:04 +0000

    Update copyright information

--- src/MULTIPR/MULTIPR_Elements.hxx
+++ src/MULTIPR/MULTIPR_Elements.hxx
@@ -1,6 +1,23 @@
-// Project MULTIPR, IOLS WP1.2.1 - EDF/CS
-// Partitioning/decimation module for the SALOME v3.2 platform
-
+//  Copyright (C) 2007-2008  CEA/DEN, EDF R&D
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+//  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+//  Partitioning/decimation module for the SALOME v3.2 platform
+//
 /**
  * \file    MULTIPR_Elements.hxx
  *
