2a17d8c6c07ac6dae76d6a65ef52b353a5179ba6
abd <abd@opencascade.com>
2008-12-04 15:07:14 +0000

    Update copyright information

--- src/MEDSPLITTER/Test/testMEDSPLITTER.py
+++ src/MEDSPLITTER/Test/testMEDSPLITTER.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2005  CEA/DEN, EDF R&D, OPEN CASCADE, PRINCIPIA R&D
+#  Copyright (C) 2007-2008  CEA/DEN, EDF R&D
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -16,7 +16,6 @@
 #
 #  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 #
-
 import os
 
 # execute Unit Test
