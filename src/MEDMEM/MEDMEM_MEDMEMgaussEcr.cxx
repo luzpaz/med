2760f7bd88da4b9015002a0cb1400ec72fad811a
abd <abd@opencascade.com>
2008-12-08 07:39:54 +0000

    Update copyright information

--- src/MEDMEM/MEDMEM_MEDMEMgaussEcr.cxx
+++ src/MEDMEM/MEDMEM_MEDMEMgaussEcr.cxx
@@ -52,24 +52,6 @@ namespace med_2_3 {
 
     extern void _MEDmodeErreurVerrouiller(void);
 
-    /*************************************************************************
-     * COPYRIGHT (C) 1999 - 2003  EDF R&D
-     * THIS LIBRARY IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
-     * IT UNDER THE TERMS OF THE GNU LESSER GENERAL PUBLIC LICENSE
-     * AS PUBLISHED BY THE FREE SOFTWARE FOUNDATION;
-     * EITHER VERSION 2.1 OF THE LICENSE, OR (AT YOUR OPTION) ANY LATER VERSION.
-     *
-     * THIS LIBRARY IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT
-     * WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF
-     * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. SEE THE GNU
-     * LESSER GENERAL PUBLIC LICENSE FOR MORE DETAILS.
-     *
-     * YOU SHOULD HAVE RECEIVED A COPY OF THE GNU LESSER GENERAL PUBLIC LICENSE
-     * ALONG WITH THIS LIBRARY; IF NOT, WRITE TO THE FREE SOFTWARE FOUNDATION,
-     * INC., 59 TEMPLE PLACE, SUITE 330, BOSTON, MA 02111-1307 USA
-     *
-     *************************************************************************/
-
     /*
      * - Nom de la fonction : MEDgaussEcr
      * - Description : Itérateur renvoyant (n°pdt,n°or), le nbre de point de GAUSS pour le type d'élément,
