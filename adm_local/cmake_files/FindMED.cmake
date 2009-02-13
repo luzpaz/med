5c6b404e95dc81f1731155e15999675bf44eaf5d
ageay <ageay>
2009-02-13 18:04:21 +0000

    *** empty log message ***

--- adm_local/cmake_files/FindMED.cmake
+++ adm_local/cmake_files/FindMED.cmake
@@ -19,6 +19,14 @@
 #
 #  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 #
-include $(top_srcdir)/adm_local/unix/make_common_starter.am
 
-SUBDIRS = unix cmake_files
+SET(MED_CXXFLAGS -I${MED_ROOT_DIR}/include/salome)
+
+FIND_LIBRARY(interpkernel interpkernel ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(medmem medmem ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(med_V2_1 med_V2_1 ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(MEDWrapperBase MEDWrapperBase ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(MEDWrapper MEDWrapper ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(MEDWrapper_V2_1 MEDWrapper_V2_1 ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(MEDWrapper_V2_2 MEDWrapper_V2_2 ${MED_ROOT_DIR}/lib/salome)
+FIND_LIBRARY(SalomeIDLMED SalomeIDLMED ${MED_ROOT_DIR}/lib/salome)
