754920a9d37c9414f0ca74288e1772f380ffd200
eap <eap@opencascade.com>
2009-08-12 14:58:00 +0000

    Parallel MED splitter
    delete fields here, not in descendant class

--- src/MEDSPLITTER/MEDSPLITTER_Graph.cxx
+++ src/MEDSPLITTER/MEDSPLITTER_Graph.cxx
@@ -20,8 +20,27 @@
 
 using namespace MEDSPLITTER;
 
-Graph::Graph(const MEDMEM::MEDSKYLINEARRAY* array, int* edgeweight):m_graph(array),m_partition(0),m_edgeweight(edgeweight),m_cellweight(0)
+Graph::Graph(MEDMEM::MEDSKYLINEARRAY* array, int* edgeweight):m_graph(array),m_partition(0),m_edgeweight(edgeweight),m_cellweight(0)
 {
-		
+}
+
+//================================================================================
+/*!
+ * \brief Destructor
+ */
+//================================================================================
+
+Graph::~Graph()
+{
+  if (m_partition)
+  {
+    delete m_partition;
+    m_partition=0;
+  }
+  if (m_graph)
+  {
+    delete m_graph;
+    m_graph=0;
+  }
 }
 
