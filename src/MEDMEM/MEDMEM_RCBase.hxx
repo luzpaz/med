455269b51dc866ccbb12ab5a7b2296cc7cf3fddd
eap <eap@opencascade.com>
2010-09-07 07:35:32 +0000

    Preparation of MEDMEM for SALOME 6x +  /*! +   * \brief Class calling RCBASE::removeReference() at distruction. +   * Useful to avoid memory leaks in case of exceptions; +   * helps not to forget to delete anything, etc. +   */ +  class MEDMEM_EXPORT AutoDeref

--- src/MEDMEM/MEDMEM_RCBase.hxx
+++ src/MEDMEM/MEDMEM_RCBase.hxx
@@ -33,10 +33,27 @@ namespace MEDMEM {
     void addReference() const;
     bool removeReference() const;
   protected:
+    void clearRefCouner();
     virtual ~RCBASE();
   private:
     mutable int _cnt;
   };
+
+  /*!
+   * \brief Class calling RCBASE::removeReference() at distruction.
+   * Useful to avoid memory leaks in case of exceptions;
+   * helps not to forget to delete anything, etc.
+   */
+  class MEDMEM_EXPORT AutoDeref
+  {
+  public:
+    AutoDeref(const RCBASE* obj);
+    ~AutoDeref();
+  private:
+    const RCBASE* _obj;
+    AutoDeref(const AutoDeref&);
+    AutoDeref& operator=(const AutoDeref&);
+  };    
 
 }
 
