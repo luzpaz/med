2a28de97b213743bfa17e7def842ef5fa6a0dbf6
eap <eap@opencascade.com>
2010-09-07 07:38:32 +0000

    Preparation of MEDMEM for SALOME 6x     Replace MESH by GMESH     MED no longer exists

--- src/MEDMEM/MEDMEM_TopLevel.cxx
+++ src/MEDMEM/MEDMEM_TopLevel.cxx
@@ -21,23 +21,21 @@
 //
 
 #include "MEDMEM_TopLevel.hxx"
-#include "MEDMEM_Med.hxx"
 #include "MEDMEM_Mesh.hxx"
 #include "MEDMEM_Field.hxx"
 #include "MEDMEM_DriverFactory.hxx"
-#include "MEDMEM_MedMedDriver.hxx"
 
 using namespace MED_EN;
 using namespace MEDMEM;
 
-MED *::MEDMEM::readMedInFile(const std::string& fileName) throw(MEDEXCEPTION)
-{
-  MED *myMedObj = new MED;
-  driverTypes type = DRIVERFACTORY::deduceDriverTypeFromFileName(fileName);
-  int id = myMedObj->addDriver(type,fileName,RDWR);
-  myMedObj->read(id);
-  return myMedObj;
-}
+// MED *::MEDMEM::readMedInFile(const std::string& fileName) throw(MEDEXCEPTION)
+// {
+//   MED *myMedObj = new MED;
+//   driverTypes type = DRIVERFACTORY::deduceDriverTypeFromFileName(fileName);
+//   int id = myMedObj->addDriver(type,fileName,RDWR);
+//   myMedObj->read(id);
+//   return myMedObj;
+// }
 
 MESH *::MEDMEM::readMeshInFile(const std::string& fileName, const std::string& meshName)
 {
@@ -48,13 +46,13 @@ MESH *::MEDMEM::readMeshInFile(const std::string& fileName, const std::string& m
   return mesh;
 }
 
-void ::MEDMEM::writeMedToFile(const MED *medObj, const std::string& fileName)
-{ 
-  MED *medObjNoC = (MED *)medObj;//MED::write should be a const method ... As it's not the case
-  driverTypes type = DRIVERFACTORY::deduceDriverTypeFromFileName(fileName);
-  int id = medObjNoC->addDriver(type,fileName,RDWR);
-  medObjNoC->write(id);
-}
+// void ::MEDMEM::writeMedToFile(const MED *medObj, const std::string& fileName)
+// { 
+//   MED *medObjNoC = (MED *)medObj;//MED::write should be a const method ... As it's not the case
+//   driverTypes type = DRIVERFACTORY::deduceDriverTypeFromFileName(fileName);
+//   int id = medObjNoC->addDriver(type,fileName,RDWR);
+//   medObjNoC->write(id);
+// }
 
 void ::MEDMEM::writeMeshToFile(const MESH *meshObj, const std::string& fileName)
 {
