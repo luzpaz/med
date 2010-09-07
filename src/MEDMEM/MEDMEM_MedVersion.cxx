27bee81bcfa19a2abc1d80814c1ce9d41a85c011
eap <eap@opencascade.com>
2010-09-07 07:09:57 +0000

    Preparation of MEDMEM for SALOME 6x    V21 is no longer supported

--- src/MEDMEM/MEDMEM_MedVersion.cxx
+++ src/MEDMEM/MEDMEM_MedVersion.cxx
@@ -22,7 +22,6 @@
 
 #include "MEDMEM_MedVersion.hxx"
 #include "MEDMEM_Utilities.hxx"
-#include "MEDMEM_Compatibility21_22.hxx"
 
 using namespace MEDMEM;
 using namespace MED_EN;
@@ -32,49 +31,49 @@ medFileVersion MEDMEM::getMedFileVersion(const string & fileName)
 {
   medFileVersion version;
 
-  med_2_1::med_idt fid21;
+  //med_2_1::med_idt fid21;
   med_2_3::med_idt fid22;
 
-  med_2_1::med_err ret21;
+  //med_2_1::med_err ret21;
   med_2_3::med_err ret22;
 
-  med_2_1::med_int major21;
-  med_2_1::med_int minor21;
-  med_2_1::med_int release21;
+//   med_2_1::med_int major21;
+//   med_2_1::med_int minor21;
+//   med_2_1::med_int release21;
 
   med_2_3::med_int major22;
   med_2_3::med_int minor22;
   med_2_3::med_int release22;
 
   med_2_3::med_mode_acces access22 = med_2_3::MED_LECTURE;
-  med_2_1::med_mode_acces access21 = med_2_1::MED_LECT;
+  //  med_2_1::med_mode_acces access21 = med_2_1::MED_LECT;
 
   /*
     Med Version 2.1 access to the file
   */
 
-  fid21 = med_2_1::MEDouvrir((const_cast <char *> (fileName.c_str())),
-                             access21);
+//   fid21 = med_2_1::MEDouvrir((const_cast <char *> (fileName.c_str())),
+//                              access21);
 
-  if (fid21 < 0)
-    throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 access");
+//   if (fid21 < 0)
+//     throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 access");
 
-  ret21 = med_2_1::MEDversionLire(fid21,&major21,&minor21,&release21);
+//   ret21 = med_2_1::MEDversionLire(fid21,&major21,&minor21,&release21);
 
-  if (ret21 < 0)
-    throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 version numbers reading");
+//   if (ret21 < 0)
+//     throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 version numbers reading");
 
-  if ((minor21 == -1) || (release21 == -1))
-    {
-      MESSAGE_MED("getMedFileVersion the file may have been produced by a version 2.1.x x<5");
-      minor21 = 1;
-      release21 = 5;
-    }
+//   if ((minor21 == -1) || (release21 == -1))
+//     {
+//       MESSAGE_MED("getMedFileVersion the file may have been produced by a version 2.1.x x<5");
+//       minor21 = 1;
+//       release21 = 5;
+//     }
 
-  ret21 = med_2_1::MEDfermer(fid21);
+//   ret21 = med_2_1::MEDfermer(fid21);
 
-  if (ret21 < 0)
-    throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 file closing");
+//   if (ret21 < 0)
+//     throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.1 file closing");
 
   /*
     Med Version 2.3 access to the file
@@ -103,18 +102,18 @@ medFileVersion MEDMEM::getMedFileVersion(const string & fileName)
   if (ret22 < 0)
     throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V2.2 file closing");
 
-  if ((major21 != major22) || (minor21 != minor22) || (release21 != release22))
-    throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V21 and V22 version numbers are different");
+//   if ((major21 != major22) || (minor21 != minor22) || (release21 != release22))
+//     throw MEDEXCEPTION("Problem in getMedFileVersion(const string &) Med file V21 and V22 version numbers are different");
 
-  MESSAGE_MED("getMedFileVersion: status version 21 of the file major " << major21 << " minor " << minor21 << " release " << release21);
+//   MESSAGE_MED("getMedFileVersion: status version 21 of the file major " << major21 << " minor " << minor21 << " release " << release21);
 
-  MESSAGE_MED("getMedFileVersion: status version 22 of the file major " << major22 << " minor " << minor22 << " release " << release22);
+//   MESSAGE_MED("getMedFileVersion: status version 22 of the file major " << major22 << " minor " << minor22 << " release " << release22);
 
-  if (major21 == 2)
+  if (major22 == 2)
     {
-      if (minor21 == 1)
+      if (minor22 == 1)
         version = V21;
-      else if (minor21 > 1)
+      else if (minor22 > 1)
         version = V22;
     }
   else
