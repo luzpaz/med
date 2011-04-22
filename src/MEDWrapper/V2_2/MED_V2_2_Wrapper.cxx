93b494c5de30f10798d2f820eefdb3b403fb77a9
eap <eap@opencascade.com>
2011-04-22 10:02:49 +0000

    untabify

--- src/MEDWrapper/V2_2/MED_V2_2_Wrapper.cxx
+++ src/MEDWrapper/V2_2/MED_V2_2_Wrapper.cxx
@@ -1406,28 +1406,28 @@ namespace MED
       TValueHolder<TString, char> aMeshName(aMeshInfo.myName);
       med_bool chgt,trsf;
       if(theGeom!=MED::ePOLYGONE && theGeom!=MED::ePOLYEDRE)
-	{
-	  return MEDmeshnEntity(myFile->Id(),
-				&aMeshName,
-				MED_NO_DT,
-				MED_NO_IT,
-				med_entity_type(theEntity),
-				med_geometry_type(theGeom),
-				MED_CONNECTIVITY,
-				med_connectivity_mode(theConnMode),
-				&chgt,
-				&trsf);
-	}
+        {
+          return MEDmeshnEntity(myFile->Id(),
+                                &aMeshName,
+                                MED_NO_DT,
+                                MED_NO_IT,
+                                med_entity_type(theEntity),
+                                med_geometry_type(theGeom),
+                                MED_CONNECTIVITY,
+                                med_connectivity_mode(theConnMode),
+                                &chgt,
+                                &trsf);
+        }
       else if(theGeom==MED::ePOLYGONE)
-	{
-	  return MEDmeshnEntity(myFile->Id(),&aMeshName,MED_NO_DT,MED_NO_IT,med_entity_type(theEntity),
-				MED_POLYGON,MED_INDEX_NODE,med_connectivity_mode(theConnMode),&chgt,&trsf)-1;
-	}
+        {
+          return MEDmeshnEntity(myFile->Id(),&aMeshName,MED_NO_DT,MED_NO_IT,med_entity_type(theEntity),
+                                MED_POLYGON,MED_INDEX_NODE,med_connectivity_mode(theConnMode),&chgt,&trsf)-1;
+        }
       else
-	{
-	  return MEDmeshnEntity(myFile->Id(),&aMeshName,MED_NO_DT,MED_NO_IT,med_entity_type(theEntity),
-				MED_POLYHEDRON,MED_INDEX_FACE,med_connectivity_mode(theConnMode),&chgt,&trsf)-1;
-	}
+        {
+          return MEDmeshnEntity(myFile->Id(),&aMeshName,MED_NO_DT,MED_NO_IT,med_entity_type(theEntity),
+                                MED_POLYHEDRON,MED_INDEX_FACE,med_connectivity_mode(theConnMode),&chgt,&trsf)-1;
+        }
     }
     
     
