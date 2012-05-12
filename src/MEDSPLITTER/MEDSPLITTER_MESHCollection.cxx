1c931433015327a822884ae40fcf1b6c4622063f
eap <eap@opencascade.com>
2012-05-12 08:51:58 +0000

    0021576: EDF 2238 MED: Medsplitter ends with SIGSEGV segmentation fault     prevent creation of faces in the mesh while creating empty groups of faces

--- src/MEDSPLITTER/MEDSPLITTER_MESHCollection.cxx
+++ src/MEDSPLITTER/MEDSPLITTER_MESHCollection.cxx
@@ -2023,13 +2023,17 @@ void MESHCollection::castSupport(const MESHCollection& old_collection, vector<co
     support->setDescription(description);
     support->setEntity(entity);
 
-    element_array[idomain].sort();
-    element_array[idomain].unique();
+    if ( !element_array[idomain].empty() ) /* if() was added for issue 0021576
+                                              to prevent creation of faces */
+      {
+        element_array[idomain].sort();
+        element_array[idomain].unique();
 
-    if (entity != MED_EN::MED_NODE)
-      support->fillFromElementList(element_array[idomain]);
-    else
-      support->fillFromNodeList(element_array[idomain]);
+        if (entity != MED_EN::MED_NODE)
+          support->fillFromElementList(element_array[idomain]);
+        else
+          support->fillFromNodeList(element_array[idomain]);
+      }
   }
 }
 
