de0104ea947ac4b95e1de0b175676989885c2888
ageay <ageay>
2012-09-19 09:07:35 +0000

    Add author

--- src/MEDCalculator/MEDCalculatorBrowserStep.hxx
+++ src/MEDCalculator/MEDCalculatorBrowserStep.hxx
@@ -16,6 +16,7 @@
 //
 // See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
+// Author : Anthony Geay (CEA/DEN)
 
 #ifndef __MEDCALCULATORBROWSERSTEP_HXX__
 #define __MEDCALCULATORBROWSERSTEP_HXX__
