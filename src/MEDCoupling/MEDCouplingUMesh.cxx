b968b48097e18037414b04f4f5cafc7c913779f6
eap <eap@opencascade.com>
2013-04-17 10:15:43 +0000

    0021856: [CEA 663] Documenting API of MEDCoupling and MEDLoader
     fix doxycomments

--- src/MEDCoupling/MEDCouplingUMesh.cxx
+++ src/MEDCoupling/MEDCouplingUMesh.cxx
@@ -576,8 +576,8 @@ void MEDCouplingUMesh::checkDeepEquivalOnSameNodesWith(const MEDCouplingMesh *ot
  * Checks if \a this and \a other meshes are geometrically equivalent with high
  * probability, else an exception is thrown. The meshes are considered equivalent if
  * (1) meshes contain the same number of nodes and the same number of elements of the
- * same types (2) three cells of the two meshes are based on coincident nodes (with a
- * specified precision).
+ * same types (2) three cells of the two meshes (first, last and middle) are based
+ * on coincident nodes (with a specified precision).
  *  \param [in] other - the mesh to compare with.
  *  \param [in] prec - the precision used to compare nodes of the two meshes.
  *  \throw If the two meshes do not match.
@@ -2542,7 +2542,7 @@ MEDCouplingUMesh *MEDCouplingUMesh::computeSkin() const throw(INTERP_KERNEL::Exc
  * Finds nodes lying on the boundary of \a this mesh.
  *  \return DataArrayInt * - a new instance of DataArrayInt holding ids of found
  *          nodes. The caller is to delete this array using decrRef() as it is no
- *          more needed 
+ *          more needed.
  *  \throw If the coordinates array is not set.
  *  \throw If the nodal connectivity of cells is node defined.
  *
