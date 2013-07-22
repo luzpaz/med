0c8719a2a71417cb33abc05270ea24513f205a9f
bruneton <bruneton>
2013-07-22 13:56:17 +0000

    CMake: FindSalomeBoost.cmake was missing for a STANDALONE compile + FindSalomeMEDFile.cmake was still refering to an invalid reference variable + doc of FindMEDFile.cmake

--- adm_local_without_kernel/cmake_files/FindSalomeBoost.cmake
+++ adm_local_without_kernel/cmake_files/FindSalomeBoost.cmake
@@ -19,10 +19,22 @@
 # Author: Adrien Bruneton
 #
 
-# Medfile detection dor Salome
+# Boost detection dor Salome
 #
 #  !! Please read the generic detection procedure in SalomeMacros.cmake !!
 #
+SET(Boost_USE_STATIC_LIBS        OFF)
+SET(Boost_USE_MULTITHREADED      ON )
+SET(Boost_USE_STATIC_RUNTIME     OFF)
+SET(Boost_NO_BOOST_CMAKE         ON)
 
-SALOME_FIND_PACKAGE_AND_DETECT_CONFLICTS(MEDFile MEDFILE_INCLUDE_DIRS 1)
+SALOME_FIND_PACKAGE_AND_DETECT_CONFLICTS(Boost Boost_INCLUDE_DIRS 1)
+IF(Boost_FOUND OR BOOST_FOUND)
+   MESSAGE(STATUS "Boost include dirs is: ${Boost_INCLUDE_DIRS}")
+ENDIF()   
 #MARK_AS_ADVANCED()
+
+## Specific definitions:
+IF(WIN32)
+  SET(BOOST_DEFINITIONS -DBOOST_DISABLE_ASSERTS)
+ENDIF()
