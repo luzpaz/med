988bf5cdd4254560abc987d0342b4eae30b08c8a
Cédric Aguerre <cedric.aguerre@edf.fr>
2016-02-19 14:55:19 +0100

    [MEDCalc] Add presentation parameters

--- src/MEDCalc/tui/medpresentation.py
+++ src/MEDCalc/tui/medpresentation.py
@@ -23,40 +23,67 @@ from medcalc.medevents import notifyGui_addPresentation
 
 __manager = medcalc.medcorba.factory.getPresentationManager()
 
-def MakeScalarMap(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
+def MakeScalarMap(proxy,
+                  viewMode=MEDCALC.VIEW_MODE_DEFAULT,
+                  displayedInfo=MEDCALC.DISPLAY_DEFAULT,
+                  scalarBarRange=MEDCALC.ScalarBarRange(MEDCALC.SCALAR_BAR_RANGE_VAL_MIN_DEFAULT,
+                                                        MEDCALC.SCALAR_BAR_RANGE_VAL_MAX_DEFAULT),
+                  colorMap=MEDCALC.COLOR_MAP_DEFAULT
+                  ):
   # Create the presentation instance in CORBA engine
   # The engine in turn creates the ParaView pipeline elements
-  params = MEDCALC.ScalarMapParameters(proxy.id, viewMode)
+  params = MEDCALC.ScalarMapParameters(proxy.id, viewMode, displayedInfo, scalarBarRange, colorMap)
   presentation_id = __manager.makeScalarMap(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
 
-def MakeContour(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
-  params = MEDCALC.ContourParameters(proxy.id, viewMode)
+def MakeContour(proxy,
+                viewMode=MEDCALC.VIEW_MODE_DEFAULT,
+                displayedInfo=MEDCALC.DISPLAY_DEFAULT,
+                scalarBarRange=MEDCALC.ScalarBarRange(MEDCALC.SCALAR_BAR_RANGE_VAL_MIN_DEFAULT,
+                                                      MEDCALC.SCALAR_BAR_RANGE_VAL_MAX_DEFAULT),
+                colorMap=MEDCALC.COLOR_MAP_DEFAULT,
+                nbContours=MEDCALC.NB_CONTOURS_DEFAULT
+                ):
+  params = MEDCALC.ContourParameters(proxy.id, viewMode, displayedInfo, scalarBarRange, colorMap, nbContours)
   presentation_id = __manager.makeContour(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
 
-def MakeVectorField(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
+def MakeVectorField(proxy,
+                    viewMode=MEDCALC.VIEW_MODE_DEFAULT
+                    ):
   params = MEDCALC.VectorFieldParameters(proxy.id, viewMode)
   presentation_id = __manager.makeVectorField(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
 
-def MakeSlices(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
-  params = MEDCALC.SlicesParameters(proxy.id, viewMode)
+def MakeSlices(proxy,
+               viewMode=MEDCALC.VIEW_MODE_DEFAULT,
+               orientation=MEDCALC.SLICE_ORIENTATION_DEFAULT,
+               nbSlices=MEDCALC.NB_SLICES_DEFAULT
+               ):
+  params = MEDCALC.SlicesParameters(proxy.id, viewMode, orientation, nbSlices)
   presentation_id = __manager.makeSlices(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
 
-def MakeDeflectionShape(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
+def MakeDeflectionShape(proxy,
+                        viewMode=MEDCALC.VIEW_MODE_DEFAULT
+                        ):
   params = MEDCALC.DeflectionShapeParameters(proxy.id, viewMode)
   presentation_id = __manager.makeDeflectionShape(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
 
-def MakePointSprite(proxy, viewMode=MEDCALC.VIEW_MODE_REPLACE):
-  params = MEDCALC.PointSpriteParameters(proxy.id, viewMode)
+def MakePointSprite(proxy,
+                    viewMode=MEDCALC.VIEW_MODE_DEFAULT,
+                    displayedInfo=MEDCALC.DISPLAY_DEFAULT,
+                    scalarBarRange=MEDCALC.ScalarBarRange(MEDCALC.SCALAR_BAR_RANGE_VAL_MIN_DEFAULT,
+                                                          MEDCALC.SCALAR_BAR_RANGE_VAL_MAX_DEFAULT),
+                    colorMap=MEDCALC.COLOR_MAP_DEFAULT,
+                    ):
+  params = MEDCALC.PointSpriteParameters(proxy.id, viewMode, displayedInfo, scalarBarRange, colorMap)
   presentation_id = __manager.makePointSprite(params)
   notifyGui_addPresentation(proxy.id, presentation_id)
 #
