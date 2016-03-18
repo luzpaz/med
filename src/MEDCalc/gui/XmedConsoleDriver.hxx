7f69d161e9db6914c639cfc48f5279ecb50f5933
vsr <vsr@opencascade.com>
2016-03-18 18:00:26 +0300

    Update according PyConsole redesign in GUI module

--- src/MEDCalc/gui/XmedConsoleDriver.hxx
+++ src/MEDCalc/gui/XmedConsoleDriver.hxx
@@ -23,7 +23,7 @@
 
 #include "MEDCALCGUI.hxx"
 
-#include <SalomePyConsole_Console.h>
+#include <PyConsole_Console.h>
 
 #include <QStringList>
 
@@ -39,7 +39,7 @@ public:
 
 private:
   MEDModule* _salomeModule;
-  SalomePyConsole_Console * _pyConsole;
+  PyConsole_Console * _pyConsole;
   bool _importXmedDone;
 };
 
